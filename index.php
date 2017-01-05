9c24117292e605600f3f5bd5e309516ffcd40af4
shawgoon <alban.havard@aliceadsl.fr>
2017-01-05 16:53:11 +0100

    users-file

--- index.php
+++ index.php
@@ -1,15 +1,18 @@
 <?php
-require ('./classes/autoload.php');
+require ('./classes/Autoload.php');
 spl_autoload_register('Autoload::classesAutoloader');
-require ('./classes/log.php');
 
-try
-{
+try {
 throw new Exception("Error Processing Request");
 
-} catch (Exception $e){
+} catch (Exception $e) {
   Log::writeCSV($e);
 }
 var_dump($e);
+$user = new Users();
+var_dump($user);
+$article = new Articles('2017-01-05 15:13:25', 'bienvenu', 'ici gît l\'article');
+var_dump($article);
 
+include ('./form/signup.php');
 ?>
